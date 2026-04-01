f276fd7a794c034b6f89067e05b2b27af31b08d5
deepalisaraswat <deepali.saraswat@oracle.com>
2026-04-01 15:40:31 +0530

    first AI-powered API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from app.ai_client import analyze_log
 
 app = FastAPI()
 
+class LogRequest(BaseModel):
+    log: str
+
 @app.get("/")
 def home():
-    return {"message": "AI DevOps Assistant running"}
+    return {"message": "AI DevOps Assistant running-AI Explorer"}
+
+@app.post("/analyze")
+def analyze(request: LogRequest):
+    result = analyze_log(request.log)
+    return {"analysis": result}
